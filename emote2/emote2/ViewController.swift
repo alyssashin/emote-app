213da5df95e6f8bf7691608b41da1af44b67d100
Alyssa Shin <alyssa.shin@duke.edu>
2019-11-02 19:05:02 -0400

    initial commit

---
+++ emote2/emote2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emote2
+//
+//  Created by Alyssa Shin on 11/2/19.
+//  Copyright © 2019 Alyssa Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
